bcf54d7379ac7626edcbb6782eef5388dfe3fc84
Sebas0399 <antony.aguay@gmail.com>
2023-07-27 09:02:55 -0500

    pokemon

--- app/src/main/java/com/example/dispositivosmoviles/UI/activities/MainActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/UI/activities/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.example.dispositivosmoviles.UI.activities
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
 import android.annotation.SuppressLint
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.speech.RecognizerIntent
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.dispositivosmoviles.R
+import com.example.dispositivosmoviles.data.utilities.MyLocationManager
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.example.dispositivosmoviles.logic.UserLogic.UserValidation
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
 import com.google.android.material.snackbar.Snackbar
 import java.util.Locale
 import java.util.UUID
@@ -27,7 +41,99 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "se
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var client: SettingsClient
+    private lateinit var locationSettingsRequest: LocationSettingsRequest
 
+
+    private var currentLocation: Location? = null
+
+    @SuppressLint("MissingPermission")
+
+    private val locationContract =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+
+
+            when (isGranted) {
+                true -> {
+                    client.checkLocationSettings(locationSettingsRequest).apply {
+                        addOnSuccessListener {
+                            val task = fusedLocationProviderClient.lastLocation
+                            task.addOnSuccessListener { location ->
+                                fusedLocationProviderClient.requestLocationUpdates(
+                                    locationRequest, locationCallback, Looper.getMainLooper()
+                                )
+                            }
+                        }
+                        addOnFailureListener {ex->
+                            if(ex is ResolvableApiException){
+                                ex.startResolutionForResult(
+                                    this@MainActivity,
+                                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED
+                                )
+                            }
+
+                        }
+
+
+                    }
+
+                }
+
+
+                shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    Snackbar.make(
+                        binding.imageView,
+                        "Ayude con el permiso porfa",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+
+                false -> {
+                    Snackbar.make(binding.imageView, "Permiso denegado", Snackbar.LENGTH_SHORT)
+                        .show()
+
+                }
+
+                else -> {}
+            }
+
+
+        }
+    private val speechToText =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+                activityResult ->
+            val sn = Snackbar.make(binding.imageView, "", Snackbar.LENGTH_SHORT)
+            var msessage = ""
+            when (activityResult.resultCode) {
+                RESULT_OK -> {
+                    val msg =
+                        activityResult.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                            ?.get(0)
+                            .toString()
+                    val intent = Intent(Intent.ACTION_WEB_SEARCH)
+                    intent.setClassName(
+                        "com.google.android.googlequicksearchbox",
+                        "com.google.android.googlequicksearchbox.SearchActivity"
+                    )
+                    intent.putExtra(SearchManager.QUERY, msg)
+                    startActivity(intent)
+                }
+
+                RESULT_CANCELED -> {
+                    msessage = "Proceso cancelado"
+                }
+
+                else -> {
+                    msessage = "Ocurrio un error"
+                }
+            }
+            sn.setText(msessage)
+            sn.show()
+        }
 
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,23 +142,47 @@ class MainActivity : AppCompatActivity() {
         //Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
+        locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY, 1000
+        ).build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                if (locationResult != null) {
+                    locationResult.locations.forEach { location ->
+                        currentLocation = location
+                        Log.d("UCE", "Ubicacion: ${location.latitude}, " + "${location.longitude}")
+                    }
+                }
+
+            }
+        }
+        client = LocationServices.getSettingsClient(this)
+        locationSettingsRequest =
+            LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build()
 
     }
 
     override fun onStart() {
         super.onStart()
         initClass()
+
+
         binding.button2.setOnClickListener {
-            //val intent=Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com.ec"))
-            //startActivity(intent)
-            val intent = Intent(Intent.ACTION_WEB_SEARCH)
-            intent.setClassName(
-                "com.google.android.googlequicksearchbox",
-                "com.google.android.googlequicksearchbox.SearchActivity"
-            )
-            intent.putExtra(SearchManager.QUERY, "UCE")
-            startActivity(intent)
+            locationContract.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+            /*  //val intent=Intent(Intent.ACTION_VIEW,Uri.parse("https://www.google.com.ec"))
+              //startActivity(intent)
+              val intent = Intent(Intent.ACTION_WEB_SEARCH)
+              intent.setClassName(
+                  "com.google.android.googlequicksearchbox",
+                  "com.google.android.googlequicksearchbox.SearchActivity"
+              )
+              intent.putExtra(SearchManager.QUERY, "UCE")
+              startActivity(intent)*/
 
         }
         val appResultLocal =
@@ -82,38 +212,7 @@ class MainActivity : AppCompatActivity() {
             }
 
 
-        val speechToText =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
-                    activityResult ->
-                val sn = Snackbar.make(binding.imageView, "", Snackbar.LENGTH_SHORT)
-                var msessage = ""
-                when (activityResult.resultCode) {
-                    RESULT_OK -> {
-                        val msg =
-                            activityResult.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                                ?.get(0)
-                                .toString()
-                        val intent = Intent(Intent.ACTION_WEB_SEARCH)
-                        intent.setClassName(
-                            "com.google.android.googlequicksearchbox",
-                            "com.google.android.googlequicksearchbox.SearchActivity"
-                        )
-                        intent.putExtra(SearchManager.QUERY, msg)
-                        startActivity(intent)
-                    }
-
-                    RESULT_CANCELED -> {
-                        msessage = "Proceso cancelado"
-                    }
-
-                    else -> {
-                        msessage = "Ocurrio un error"
-                    }
-                }
-                sn.setText(msessage)
-                sn.show()
-            }
         binding.btnResult.setOnClickListener {
             //val resIntent = Intent(this, ResultActivity::class.java)
             //appResultLocal.launch(resIntent)
@@ -176,5 +275,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    override fun onPause() {
+        super.onPause()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+    private fun test(){
+        var location=MyLocationManager(this)
+        location.getUserLocation()
+    }
 }
---
+++ app/src/main/java/com/example/dispositivosmoviles/data/utilities/MyLocationManager.kt
@@ -0,0 +1,23 @@
+package com.example.dispositivosmoviles.data.utilities
+
+import android.content.Context
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.SettingsClient
+
+class MyLocationManager (val      context:Context
+){
+    init {
+
+    }
+    private lateinit var client:SettingsClient
+    private fun initVars(){
+        if(context!=null){
+            client=LocationServices.getSettingsClient(context!!)
+        }
+    }
+     fun getUserLocation(){
+        initVars()
+
+
+    }
+}
